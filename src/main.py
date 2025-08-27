15dccbbd990c7d9ba5b11ea06bcb898a958911dd
Nikolai Volosiankov <wol87@ya.ru>
2025-08-27 11:25:09 +0200

    Add frontend

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+api_router = APIRouter(prefix="/frontend-api")
+
+
+@api_router.get("/hello")
+async def hello():
+    return {"message": "Привет из бекенда!"}
+
 
 app = FastAPI()
+app.include_router(api_router)
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+@app.get("/frontend-settings.json")
+async def frontend_settings():
+    return FileResponse("frontend-settings.json", media_type="application/json")
 
-@app.get("/test")
-def read_test():
-    return {"message": "Test"}
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
+app.mount("/assets", StaticFiles(directory="frontend/assets"), name="assets")
