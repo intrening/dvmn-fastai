cdbac91bd631b2b069743daaa1fb47a0259a0fe5
Nikolai Volosiankov <wol87@ya.ru>
2025-08-29 12:45:40 +0200

    Add user mock

--- src/main.py
+++ src/main.py
@@ -10,6 +10,18 @@ async def hello():
     return {"message": "Привет из бекенда!"}
 
 
+@api_router.get("/users/me")
+async def me():
+    return {
+        "email": "example@example.com",
+        "is_active": True,
+        "profile_id": "1",
+        "registered_at": "2025-06-15T18:29:56+00:00",
+        "updated_at": "2025-06-15T18:29:56+00:00",
+        "username": "user123",
+    }
+
+
 app = FastAPI()
 app.include_router(api_router)
 
